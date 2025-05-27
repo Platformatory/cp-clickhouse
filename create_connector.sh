fd968a90679fc957329d8162f817420469e539db
Avinash Upadhyaya <avinash@platformatory.com>
2025-05-27 13:32:34 +0530

    add user for clickhouse

--- create_connector.sh
+++ create_connector.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 curl -X POST -H "Content-Type: application/json" -d @clickhouse-connector.json localhost:8083/connectors
+
+sleep 5
+
+echo "Checking connector status..."
+
+curl localhost:8083/connectors/voltage/status
